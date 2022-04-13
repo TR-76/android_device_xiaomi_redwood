434f4c642547fcced1507ad0fcf005faee5d5993
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 16:25:29 -0700

    redwood: Drop PRODUCT_BUILD_SUPER_PARTITION
     * Super image is not generated by a default 'm' build,
       but we at least want it for dist builds.
    
    Change-Id: Ia12a5314632509fcd309d51ddf757efdc37499c3
    
    Signed-off-by: Harshit Rai <harshitrai@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -404,7 +404,6 @@ PRODUCT_PACKAGES += \
     vendor_dsp_mountpoint \
     vendor_firmware_mnt_mountpoint
 
-PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Pixel Features Spoof (for Google Photos)
