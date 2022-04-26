18422c619de392c89be4b3877b3f5d31362ca477
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-04-25 20:56:35 -0400

    redwood: configs: Import lahaina msm_irqbalance.conf
    Change-Id: I5a5e6c7770010b1e4e5aa54c3f95a8a20aa2c2f7

--- device.mk
+++ device.mk
@@ -433,7 +433,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf 
 
 # QTI
 PRODUCT_PACKAGES += \
