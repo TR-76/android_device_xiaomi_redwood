9de838e4a2e9fb09c0df0eb3255d92c0f7847702
Drpto <dr.juniornoob@gmail.com>
2023-09-29 22:41:22 +0530

    redwood: Add Blur & QuickTap support

--- lineage_redwood.mk
+++ lineage_redwood.mk
@@ -17,6 +17,10 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from MindTheGapps
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 
+# Device props
+TARGET_SUPPORTS_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
 PRODUCT_BRAND := POCO
 PRODUCT_DEVICE := redwood
 PRODUCT_MANUFACTURER := Xiaomi
