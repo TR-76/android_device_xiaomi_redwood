d4a72ec7fd9fcfd7a1557dff5b3ad77ada84c47c
TheStrechh <thestrechh@gmail.com>
2023-12-18 07:10:23 +0530

    sm8350-common: Add missing packages for Miui Camera

--- common.mk
+++ common.mk
@@ -125,7 +125,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    libcamera2ndk_vendor \
+    libion.vendor \
+    libgui_vendor
 
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
@@ -154,6 +157,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
 
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@3.0 \
     android.hardware.graphics.allocator@3.0.vendor \
     vendor.display.config@1.15.vendor \
     vendor.display.config@2.0.vendor
@@ -236,7 +240,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service
 
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
+    libhidltransport \
     libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor
 
 # HotwordEnrollement
