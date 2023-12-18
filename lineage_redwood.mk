8a4eda5a075fa620a8577350ce10068efc902eb3
TR-76 <tr-76@gmx.at>
2023-12-18 16:27:55 +0100

    Add microG
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- lineage_redwood.mk
+++ lineage_redwood.mk
@@ -14,6 +14,10 @@ $(call inherit-product, device/xiaomi/redwood/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# microG
+$(call inherit-product-if-exists, vendor/microg/config.mk)
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
 PRODUCT_BRAND := POCO
 PRODUCT_DEVICE := redwood
 PRODUCT_MANUFACTURER := Xiaomi
