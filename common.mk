LineageOS/android_device_xiaomi_sm8350-common

sm8350-common: Build missing libraries for 14 QPR3
Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

common.mk
@@ -32,6 +32,9 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
+    libaudioroute.vendor
+
+PRODUCT_PACKAGES += \
     audioadsprpcd \
     audio.r_submix.default \
     audio.usb.default
@@ -127,6 +130,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
     libmegface
+
+PRODUCT_PACKAGES += \
+    liblz4.vendor \
+    libpng.vendor \
+    libutilscallstack.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
@@ -430,6 +438,12 @@ PRODUCT_PACKAGES += \
     libjson \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl
+
+PRODUCT_PACKAGES += \
+    libcurl.vendor \
+    libjsoncpp.vendor \
+    libsqlite.vendor \
+    libssl.vendor
 
 # Sensors
 PRODUCT_PACKAGES += \

TR-76/android_device_xiaomi_redwood

common.mk
@@ -29,6 +29,7 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Audio
 PRODUCT_PACKAGES += \
+    libaudioroute.vendor \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
@@ -136,7 +137,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libion.vendor \
-    libgui_vendor
+    libgui_vendor \
+    libexif.vendor \
+    liblz4.vendor
 
 PRODUCT_PACKAGES += \
     liblz4.vendor
@@ -444,7 +447,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcurl.vendor \
     libjsoncpp.vendor \
-    libsqlite.vendor
+    libnetutils.vendor \
+    libsqlite.vendor \
+    libutilscallstack.vendor
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -513,7 +518,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service-qti \
+    libusbhost.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \