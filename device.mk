299e5f479f5743ea4696b69f0c1a51146c7c4dce
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-02-14 23:48:24 +0600

    redwood: Remove custom task_profiles
    We'll use from system_core since our API is 31 now, which has
    uclamp support.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Harshit Rai <heyharshitrai@gmail.com>

--- device.mk
+++ device.mk
@@ -414,7 +414,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.hwui.use_hint_manager=true
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 $(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-xiaomi)
