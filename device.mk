32db0214672d507a597e0c22c4af2cec44ac9690
Silesh K Nair <sileshn@gmail.com>
2025-05-06 07:48:11 +0530

    redwood: Move QTI telephony permissions to vendor

--- device.mk
+++ device.mk
@@ -359,8 +359,7 @@ $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-batte
 
 # QTI
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sysconfig/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/configs/permissions/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext-privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/sysconfig/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # Recovery
 PRODUCT_COPY_FILES += \
