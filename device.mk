b93880fb00b7e8d156161ff55d72e6806ffd5654
Giovanni Ricca <giovanniricca@proton.me>
2025-06-11 12:19:11 +0200

    redwood: Convert overlays to RROs
    Co-authored-by: Alexander Winkowski <dereference23@outlook.com>
    
    Change-Id: Iebb0fef5432a129bc9a023488927b4c1e014c98e
    Signed-off-by: Harshit Rai <heyharshitrai@gmail.com>

--- device.mk
+++ device.mk
@@ -364,20 +364,26 @@ $(foreach sku_out, $(TARGET_COPY_OUT_NFC_SKU_PERMISSIONS), \
  endif
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+PRODUCT_PACKAGES += \
+    ApertureOverlayRedwood \
+    CarrierConfigOverlayRedwood \
+    LineageSDKOverlayRedwood \
+    TelephonyOverlayRedwood
+
+PRODUCT_PACKAGES += \
+    FrameworkOverlayRedwood \
+    SettingsOverlayRedwood \
+    SettingsProviderOverlayRedwoodCN \
+    SettingsProviderOverlayRedwoodGL \
+    SettingsProviderOverlayRedwoodIN \
+    SimpleDeviceConfigOverlayRedwood \
+    SystemUIOverlayRedwood \
+    WifiOverlayRedwood \
+    WifiOverlayRedwoodCN \
+    WifiOverlayRedwoodGL \
+    WifiOverlayRedwoodIN
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-
-# Overlays-RRO
-PRODUCT_PACKAGES += \
-    redwoodCNSettingsProviderOverlay \
-    redwoodCNWifiOverlay \
-    redwoodGLSettingsProviderOverlay \
-    redwoodGLWifiOverlay \
-    redwoodINSettingsProviderOverlay \
-    redwoodINWifiOverlay
 
 # Partitions
 PRODUCT_PACKAGES += \
@@ -418,10 +424,6 @@ PRODUCT_COPY_FILES += \
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
-
-# RIL
-PRODUCT_PACKAGES += \
-    CarrierConfigOverlay
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -531,7 +533,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
     libwifi-hal-qcom \
-    WifiOverlay \
     wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf
