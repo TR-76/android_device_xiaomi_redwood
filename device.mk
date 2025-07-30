73a325affbf447e83992e7e4cb7c23da3247b9d3
kyasu <kyasu.dev@gmail.com>
2025-07-30 23:06:15 +0900

    redwood: Disable `thumbnail_block_model` of libstagefright
    Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c
    Signed-off-by: Harshit Rai <heyharshitrai@gmail.com>

--- device.mk
+++ device.mk
@@ -275,6 +275,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/etc/media_codecs_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_lahaina.xml \
     $(LOCAL_PATH)/media/etc/media_codecs_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 # Native libraries whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
