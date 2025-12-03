3e758e8806bc5686b1a3c2cab2d37114798665ed
Harshit Rai <heyharshitrai@gmail.com>
2025-12-03 04:02:00 +0000

    redwood: Drop prebuilt displayfeature interface
    vendor.xiaomi.hardware.displayfeature@1.0.so is already built from source.
    
    Signed-off-by: Harshit Rai <heyharshitrai@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -41,7 +41,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.diaghal@1.0',
         'vendor.qti.hardware.wifidisplaysession@1.0',
         'vendor.qti.imsrtpservice@3.0',
-        'vendor.xiaomi.hardware.displayfeature@1.0',
     ): lib_fixup_vendor_suffix,
 }
 
