737f3909b0b1606857875df83124cc9449a4b508
Silesh K Nair <sileshn@gmail.com>
2025-12-16 03:40:24 +0100

    redwood: add libjsoncpp_shim for libmisight.so

--- extract-files.py
+++ extract-files.py
@@ -93,7 +93,7 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('remote_handle_close')
         .clear_symbol_version('remote_handle_invoke')
         .clear_symbol_version('remote_handle_open'),
-    'vendor/lib64/libsensor_cal_v2.so': blob_fixup()
+    ('vendor/lib64/libsensor_cal_v2.so', 'vendor/lib64/libmisight.so'): blob_fixup()
         .add_needed('libjsoncpp_shim.so'),
     ('vendor/lib/hw/audio.primary.lahaina.so', 'vendor/lib/libaudioroute_ext.so'): blob_fixup()
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
