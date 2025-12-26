c57b65d83fa6e093aa5a7fcda517b43ce2613032
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-12-26 23:06:49 +0600

    redwood: Inherit Dolby Atmos of OnePlus
    Source: https://github.com/frost-testzone/vendor_oneplus_dolby/tree/abc3f6dbd3d06fd
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Inherit Dolby Atmos
+$(call inherit-product, vendor/oneplus/dolby/dolby.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 31
 
