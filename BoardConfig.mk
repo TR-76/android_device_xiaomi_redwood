Redwood-trees/device_xiaomi_redwood

redwood: fixing connecting BLE (bt 5.x) devices (smart watches, etc.)
Signed-off-by: Thereache <thereache2003@gmail.com>

BoardConfig.mk
@@ -50,6 +50,9 @@ TARGET_PROVIDES_AUDIO_EXTNS := true
 # Assert
 TARGET_OTA_ASSERT_DEVICE := redwood|redwoodin
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 

TR-76/android_device_xiaomi_redwood

BoardConfig.mk
@@ -40,6 +40,9 @@ AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
 BOARD_SUPPORTS_SOUND_TRIGGER := true
 TARGET_PROVIDES_AUDIO_EXTNS := true
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth/include
+
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := redwood
 