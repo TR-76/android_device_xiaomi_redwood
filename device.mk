08ba9dd53d4ba1c73fe0e9f2d40d2308472dc8e2
TR-76 <tr-76@gmx.net>
2026-01-27 13:22:06 +0100

    remove BCR
    Signed-off-by: TR-76 <tr-76@gmx.net>

--- device.mk
+++ device.mk
@@ -21,9 +21,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/redwood/redwood-vendor.mk)
 
-# Basic Call Recorder
-$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
-
 # MIUI Camera
 $(call inherit-product-if-exists, vendor/xiaomi/redwood-miuicamera/device.mk)
 
