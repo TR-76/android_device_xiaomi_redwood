573403e79e95d1ad28752f82c4ed0c036ff6e6f7
TR-76 <tr-76@gmx.net>
2026-01-27 13:20:44 +0100

    include Firmware if exists
    Signed-off-by: TR-76 <tr-76@gmx.net>

--- lineage_redwood.mk
+++ lineage_redwood.mk
@@ -19,6 +19,9 @@ $(call inherit-product-if-exists, vendor/cromite/cromite.mk)
 $(call inherit-product-if-exists, vendor/microg/config.mk)
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
+# Firmware
+$(call inherit-product-if-exists, vendor/xiaomi/redwood-firmware/config.mk)
+
 # AxionOS flags
 AXION_CAMERA_REAR_INFO := 108,8,2
 AXION_CAMERA_FRONT_INFO := 16
