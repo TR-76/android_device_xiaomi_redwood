faris-ae/device

redwood: Silence some spammy logging
HWC and cit sensor logspam is so intense it bumps up logd's idle CPU
usage up to over 3%. With this change, logs are much cleaner and logd
idle CPU usage is reduced to under 1%, while still allowing error logs
to pass through.
- Do this only in user/debug builds (except eng).
- Add wifi and cnss too while we're at it, they can get noisy at times.

device.mk
@@ -14,6 +14,27 @@ $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.nfc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.nfc.rc
 
+# Logging
+ SPAMMY_LOG_TAGS := \
+     MiStcImpl \
+     SDM \
+     SDM-histogram \
+     SRE \
+     WifiHAL \
+     cnss-daemon \
+     libcitsensorservice@2.0-impl \
+     libsensor-displayalgo \
+     libsensor-parseRGB \
+     libsensor-ssccalapi \
+     sensors \
+     vendor.qti.hardware.display.composer-service \
+     vendor.xiaomi.sensor.citsensorservice@2.0-service
+ 
+ ifneq ($(TARGET_BUILD_VARIANT),eng)
+ PRODUCT_VENDOR_PROPERTIES += \
+     $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+ endif
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \

TR-76/android_device_xiaomi_redwood

device.mk
@@ -314,6 +314,27 @@ $(foreach sku_out, $(TARGET_COPY_OUT_NFC_SKU_PERMISSIONS), \
         frameworks/native/data/etc/com.android.nfc_extras.xml:$(sku_out)/com.android.nfc_extras.xml \
         frameworks/native/data/etc/com.nxp.mifare.xml:$(sku_out)/com.nxp.mifare.xml))
 
+# Logging
+ SPAMMY_LOG_TAGS := \
+     MiStcImpl \
+     SDM \
+     SDM-histogram \
+     SRE \
+     WifiHAL \
+     cnss-daemon \
+     libcitsensorservice@2.0-impl \
+     libsensor-displayalgo \
+     libsensor-parseRGB \
+     libsensor-ssccalapi \
+     sensors \
+     vendor.qti.hardware.display.composer-service \
+     vendor.xiaomi.sensor.citsensorservice@2.0-service
+
+ ifneq ($(TARGET_BUILD_VARIANT),eng)
+ PRODUCT_VENDOR_PROPERTIES += \
+     $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=W)
+ endif
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \