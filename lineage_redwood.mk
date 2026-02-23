thepriyanshujangid/unified_device_xiaomi_redwood

redwood: Import device-specific blobs from OS2.0.7.0.UMSMIXM
Signed-off-by: thepriyanshujangid <priyanshujangid@yahoo.com>

lineage_redwood.mk
@@ -26,9 +26,9 @@ PRODUCT_SYSTEM_NAME := redwood_global
 PRODUCT_SYSTEM_DEVICE := redwood
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redwood_global-user 13 RKQ1.211001.001 V14.0.2.0.TMSMIXM release-keys" \
+    PRIVATE_BUILD_DESC="redwood_global-user 14 UKQ1.240624.001 OS2.0.7.0.UMSMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := POCO/redwood_global/redwood:13/RKQ1.211001.001/V14.0.2.0.TMSMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/redwood_global/redwood:14/UKQ1.240624.001/OS2.0.7.0.UMSMIXM:user/release-keys

TR-76/android_device_xiaomi_redwood

Signed-off-by: Harshit Rai <heyharshitrai@gmail.com>

lineage_redwood.mk
@@ -26,7 +26,7 @@ PRODUCT_SYSTEM_NAME := redwood_global
 PRODUCT_SYSTEM_DEVICE := redwood
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="redwood_global-user 13 RKQ1.211001.001 V14.0.2.0.TMSMIXM release-keys" \
-    BuildFingerprint=POCO/redwood_global/redwood:13/RKQ1.211001.001/V14.0.2.0.TMSMIXM:user/release-keys \
+    BuildDesc="redwood_global-user 14 UKQ1.240624.001 OS2.0.7.0.UMSMIXM release-keys" \
+    BuildFingerprint=POCO/redwood_global/redwood:14/UKQ1.240624.001/OS2.0.7.0.UMSMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)