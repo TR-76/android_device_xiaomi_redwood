d9c18fbb30ffd34ab4e2fb7a0a489a8f0194d8a7
realahnet <syedmusicrealise@gmail.com>
2026-02-28 19:30:05 +0500

    redwood: Inherit Sony Dolby
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Harshit Rai <heyharshitrai@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,4 +224,5 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit proprietary blobs
+-include vendor/sony/dolby/BoardConfigDolby.mk
 include vendor/xiaomi/redwood/BoardConfigVendor.mk
