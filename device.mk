a89e7659a119824658a4ceda1700e1cc9dc29ff0
Harshit Rai <heyharshitrai@gmail.com>
2026-03-20 22:59:11 +0000

    Revert "redwood: Inherit Dolby Atmos of OnePlus"
    This reverts commit 5ed1e8b8c1e298fce0b720ef06d62859ed081620.

--- device.mk
+++ device.mk
@@ -33,9 +33,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-# Inherit Dolby Atmos
-$(call inherit-product, vendor/oneplus/dolby/dolby.mk)
-
 # API
 PRODUCT_SHIPPING_API_LEVEL := 31
 
