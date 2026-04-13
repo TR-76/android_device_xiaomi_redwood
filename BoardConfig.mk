aed0057547e0064980e85d1c8c2da0b20ee7c7b4
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2026-04-13 07:21:01 +0000

    redwood: Inherit Xiaomi Dolby Atmos with Lunaris Dolby UI
    - Enable Xiaomi Dolby Atmos with Lunaris Dolby UI for redwood
    - Add '<Include href="media_codecs_dolby_audio.xml" />' in media codecs config
    - Add the Dolby and spatial audio audio-policy/effects changes
    - Add Bluetooth routing support for Dolby processing
    - Drop props that disable Dolby
    
    Squash of:
    - https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/e2333c94444b2646810cca39926f2df6bb95443c
    - https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/07b4e9115a6346b3bbf34ba6cf96909f91f40a30
    - https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/6633f66fb105e0c00440d2909d460ec2918cfb38
    - https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/aed140439899dbe562cf407327e4ec8bc0b5a2a7
    
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: Harshit Rai <heyharshitrai@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
-DEVICE_MANIFEST_FILE := \
+DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/hidl/manifest_lahaina.xml \
     $(DEVICE_PATH)/hidl/manifest_xiaomi.xml \
     $(if $(TARGET_NFC_SUPPORTED_SKUS),$(DEVICE_PATH)/hidl/manifest_no_nfc.xml,)
--- device.mk
+++ device.mk
@@ -72,6 +72,12 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
+PRODUCT_PACKAGES += \
+    LunarisDolby
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_io_policy.conf \
@@ -81,9 +87,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/audio_io_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/audio_policy_configuration.xml
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
