d8efe11b4882b2dd904a904c8fb2789a7cd9c14e
Harshit Rai <heyharshitrai@gmail.com>
2026-04-13 06:56:14 +0000

    redwood: Drop Sony Dolby
    Signed-off-by: Harshit Rai <heyharshitrai@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -228,5 +228,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit proprietary blobs
--include vendor/sony/dolby/BoardConfigDolby.mk
 include vendor/xiaomi/redwood/BoardConfigVendor.mk
