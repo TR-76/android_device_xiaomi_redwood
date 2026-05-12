cdc52cbb9a2c562bd0a5aa8d360b4bfda23a4dbb
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-05-12 12:39:52 +0000

    redwood: Introduce init.lahaina.perf.rc for performance tuning
    Add a lahaina family specific init file for performance management:
    - Boot-time and runtime filesystem I/O tuning
    - Runtime cpuset configuration for all process groups
    - UCLAMP and CPU shares tuning for background throttling
    - Disable multiple kswapd threads
    - Enable deep suspend to RAM
    - Dynamically set IRQ affinity for KGSL and MSM DRM
      (bind kgsl_3d0_irq to CPU 1, msm_drm to CPU 2)
    - Enable PowerHAL hint processing
    - Optimize dex2oat CPU set on device provisioning
    
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-Authored-By: Alex Naidis <alex.naidis@paranoidandroid.co>
    Co-Authored-By: Jake Weinstein <jake@aospa.co>
    Signed-off-by: Harshit Rai <heyharshitrai@gmail.com>

--- device.mk
+++ device.mk
@@ -258,6 +258,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.batterysecret.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.batterysecret.rc \
     $(LOCAL_PATH)/rootdir/etc/init.mi_thermald.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.mi_thermald.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.lahaina.perf.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.lahaina.perf.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qti.kernel.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qti.kernel.rc \
     $(LOCAL_PATH)/rootdir/etc/init.redwood.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.redwood.rc \
@@ -265,6 +266,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.kernel.post_boot-yupik.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.kernel.post_boot-yupik.sh \
+    $(LOCAL_PATH)/rootdir/bin/init.lahaina.perf.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.lahaina.perf.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh \
     $(LOCAL_PATH)/rootdir/bin/vendor_modprobe.sh:$(TARGET_COPY_OUT_VENDOR)/bin/vendor_modprobe.sh
 
---
+++ rootdir/bin/init.lahaina.perf.sh
@@ -0,0 +1,15 @@
+#!/vendor/bin/sh
+#
+# Copyright (C) 2024 Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function write_irq_affinity() {
+    irq_dir="$(dirname /proc/irq/*/$1)"
+    [ -d "$irq_dir" ] && echo $2 > "${irq_dir}/smp_affinity_list"
+}
+
+# kgsl_3d0_irq -> CPU 1
+# msm_drm -> CPU 2
+write_irq_affinity kgsl_3d0_irq 1
+write_irq_affinity msm_drm 2
